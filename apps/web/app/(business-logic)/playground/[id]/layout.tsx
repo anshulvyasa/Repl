076a606a7b0c52443a7da54b4ee7e5e7f51e830d
anshulvyasa <anshulvtech121@gmail.com>
2025-10-21 19:52:41 +0000

    starter and path to json added

---
+++ apps/web/app/(business-logic)/playground/[id]/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlaygroundLayout = ({ children }: { children: React.ReactNode }) => {
+  return <main>{children}</main>;
+};
+
+export default PlaygroundLayout;
---
+++ apps/web/app/(business-logic)/playground/[id]/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useParams } from "next/navigation";
+
+const Playground = () => {
+  const { id } = useParams<{ id: string }>();
+  return <div>The Current ID is {id}</div>;
+};
+
+export default Playground;
