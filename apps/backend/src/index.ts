aadbdffd8a14b32d4f72523e3e84de0ad7ced26b
anshulvyasa <anshulvtech121@gmail.com>
2025-12-18 16:53:05 +0000

    github action and related logic updated

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.ORIGIN,
     credentials: true,
   })
 );
